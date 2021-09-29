6fdac7e317a091bd65039d6d23e053b0f8b1944c
Niranj <srinivasniranj@gmail.com>
2021-09-29 13:18:12 -0400

    calculate percent replicating for dl profiles

--- benchmark/utils.py
+++ benchmark/utils.py
@@ -3,6 +3,11 @@ import glob
 import pandas as pd
 import numpy as np
 import random
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils import check_array, as_float_array
+from sklearn.base import TransformerMixin, BaseEstimator
+import kneed
+import scipy
 
 
 def load_data(exp, plate, filetype):
@@ -212,3 +217,78 @@ def null_correlation_between_modalities(modality_1_df, modality_2_df, modality_1
         count += 1
 
     return null_modalities
+
+
+class ZCA_corr(BaseEstimator, TransformerMixin):
+    def __init__(self, copy=False):
+        self.copy = copy
+
+    def estimate_regularization(self, eigenvalue):
+        x = [_ for _ in range(len(eigenvalue))]
+        kneedle = kneed.KneeLocator(x, eigenvalue, S=1.0, curve='convex', direction='decreasing')
+        reg = eigenvalue[kneedle.elbow]/10.0
+        return reg # The complex part of the eigenvalue is ignored
+
+    def fit(self, X, y=None):
+        """
+        Compute the mean, sphering and desphering matrices.
+        Parameters
+        ----------
+        X : array-like with shape [n_samples, n_features]
+            The data used to compute the mean, sphering and desphering
+            matrices.
+        """
+        X = check_array(X, accept_sparse=False, copy=self.copy, ensure_2d=True)
+        X = as_float_array(X, copy=self.copy)
+        self.mean_ = X.mean(axis=0)
+        X_ = X - self.mean_
+        cov = np.dot(X_.T, X_) / (X_.shape[0] - 1)
+        V = np.diag(cov)
+        df = pd.DataFrame(X_)
+        corr = np.nan_to_num(df.corr()) # replacing nan with 0 and inf with large values
+        G, T, _ = scipy.linalg.svd(corr)
+        regularization = self.estimate_regularization(T.real)
+        t = np.sqrt(T.clip(regularization))
+        t_inv = np.diag(1.0 / t)
+        v_inv = np.diag(1.0/np.sqrt(V.clip(1e-3)))
+        self.sphere_ = np.dot(np.dot(np.dot(G, t_inv), G.T), v_inv)
+        return self
+
+    def transform(self, X, y=None, copy=None):
+        """
+        Parameters
+        ----------
+        X : array-like with shape [n_samples, n_features]
+            The data to sphere along the features axis.
+        """
+        check_is_fitted(self, "mean_")
+        X = as_float_array(X, copy=self.copy)
+        return np.dot(X - self.mean_, self.sphere_.T)
+
+
+def sphere_plate_zca_corr(plate):
+    """
+    sphere each plate to the DMSO negative control values
+    Parameters:
+    -----------
+    plate: pandas.DataFrame
+        dataframe of a single plate's featuredata and metadata
+    Returns:
+    -------
+    pandas.DataFrame of the same shape as `plate`
+    """
+    # sphere featuredata to DMSO sphering matrix
+    spherizer = ZCA_corr()
+    dmso_df = plate.loc[plate.Metadata_control_type=="negcon"]
+    dmso_vals = get_featuredata(dmso_df).to_numpy()
+    all_vals = get_featuredata(plate).to_numpy()
+    spherizer.fit(dmso_vals)
+    sphered_vals = spherizer.transform(all_vals)
+    # concat with metadata columns
+    feature_df = pd.DataFrame(
+        sphered_vals, columns=get_featurecols(plate), index=plate.index
+    )
+    metadata = get_metadata(plate)
+    combined = pd.concat([feature_df, metadata], axis=1)
+    assert combined.shape == plate.shape
+    return combined
